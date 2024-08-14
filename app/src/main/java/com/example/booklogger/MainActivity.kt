da1c51abbe16a5e97952c4e531a4f2646e3c50e1
Milan Mishra <milanmishr20@gmail.com>
2024-08-14 01:11:55 -0500

    Second page

--- app/src/main/java/com/example/booklogger/MainActivity.kt
+++ app/src/main/java/com/example/booklogger/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.booklogger.ui.theme.BookLoggerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             BookLoggerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Book Logger",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,11 +35,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(-25.dp)) {
+        Text(
+            text = "Welcome to $name!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Recently Read",
+            modifier = modifier
+        )
+    }
 }
+
+@Composable
+fun NewPage(){
+    Text(text = "This is a new page")
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
